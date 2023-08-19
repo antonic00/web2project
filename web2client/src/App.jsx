6da74356528ba29bd6ad3be5f8c2a125b6b21eef
antonic <aleksandar.antonic00@gmail.com>
2023-08-19 18:53:56 +0200

    Handle user registration with redux

--- web2client/src/App.jsx
+++ web2client/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Header />
-      <Container>
+      <Container className="mt-3">
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
--- web2client/src/index.js
+++ web2client/src/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
