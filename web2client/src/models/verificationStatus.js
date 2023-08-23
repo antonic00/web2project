9645bab69097c3366bae70990dc24a715cb26186
antonic <aleksandar.antonic00@gmail.com>
2023-08-23 20:56:53 +0200

    Add modal with verification form

--- web2client/src/models/verificationStatus.js
+++ web2client/src/models/verificationStatus.js
@@ -1,15 +1,20 @@
 export const VerificationStatus = {
-    Pending: 0,
-    Rejected: 1,
-    Accepted: 2,
-  };
-  
-  export function statusIdToName(statusId) {
-    return Object.keys(VerificationStatus).find(
-      (key) => VerificationStatus[key] === statusId
-    );
-  }
-  
-  export function statusNameToId(statusName) {
-    return VerificationStatus[statusName];
-  }
+  Pending: 0,
+  Rejected: 1,
+  Accepted: 2,
+};
+
+export const AllowedStatuses = [
+  VerificationStatus.Rejected,
+  VerificationStatus.Accepted,
+];
+
+export function statusIdToName(statusId) {
+  return Object.keys(VerificationStatus).find(
+    (key) => VerificationStatus[key] === statusId
+  );
+}
+
+export function statusNameToId(statusName) {
+  return VerificationStatus[statusName];
+}
